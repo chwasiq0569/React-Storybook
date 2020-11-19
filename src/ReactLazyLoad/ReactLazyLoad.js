2c65602011e58a28100d8f1fd7300f26594413bd
unknown <samsunggrandprimeplus056@gmail.com>
2020-11-18 20:58:16 -0800

    ReactLazyLoad

--- src/ReactLazyLoad/ReactLazyLoad.js
+++ src/ReactLazyLoad/ReactLazyLoad.js
@@ -5,7 +5,9 @@ const ReactLazyLoad = () => {
   return (
     <div>
       <h1>React Lazy Loading Code Splitting</h1>
-
+      <Suspense fallback={<div>LazyAboutPage is Loading</div>}>
+        <LazyAboutPage />
+      </Suspense>
       <Suspense fallback={<div>Home is Loading</div>}>
         <LazyHome />
       </Suspense>
