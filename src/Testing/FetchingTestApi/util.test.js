2853aa130941b1370472f4a3e2ed724b359a1143
unknown <samsunggrandprimeplus056@gmail.com>
2020-12-10 04:04:49 -0800

    Tested util.js Module

--- src/Testing/FetchingTestApi/util.test.js
+++ src/Testing/FetchingTestApi/util.test.js
@@ -1,4 +1,5 @@
 import fetchMock from "fetch-mock";
+import { fetchUser } from "./util";
 
 describe("Tesing Util Module", () => {
   it("fetchUrl function Tests Passing", async () => {
@@ -8,5 +9,9 @@ describe("Tesing Util Module", () => {
       name: "James Franklin",
       website: "www.jamesfranklin.com",
     };
+    //wehn we are not tesitn for error cases we cannot specity in the fetchMock it run test for success it own
+    fetchMock.getOnce(url, dummyUser);
+    const response = await fetchUser(1);
+    expect(response).toEqual(dummyUser);
   });
 });
